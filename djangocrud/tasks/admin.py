1a34daa15f1fe9e4ec8bead4f178f90fbd55be83
aramirez007 <168164179+aramirez007@users.noreply.github.com>
2024-04-26 21:17:00 -0600

    Delete djangocrud directory

--- djangocrud/tasks/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Tarea
-# Register your models here.
-
-class TareaAdmin(admin.ModelAdmin):
-    readonly_fields = ("creado", )
-
-
-admin.site.register(Tarea, TareaAdmin)
